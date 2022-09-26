c2f29699cb233c661eeb1b672e896e1acb9954a4
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-09-26 20:13:56 +0000

    mocha and chai added

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,14 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-// Test Case 1: Check the returned array elements
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case2: Check the original array
 let words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  
+  it("returns original array", () => {
+    assert.strictEqual(words.length, 3);
+  })
+});
+
+
